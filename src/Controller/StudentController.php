d375b0d0d8c4808a6ffdfd4cb48ddbee9251387c
mr4dd <158076664+mr4dd@users.noreply.github.com>
2025-12-20 13:57:15 +0100

    UI for courses and student pages, no logic yet

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class StudentController extends AbstractController
+{
+    #[Route('/students', name: 'app_student')]
+    public function index(): Response
+    {
+        return $this->render('student/index.html.twig', [
+            'controller_name' => 'StudentController',
+        ]);
+    }
+}
